1a97d7ddad9f5f3437139ae50968ad0a074f550c
Ron Young <ron@ryoung.io>
2024-08-17 10:14:16 -0500

    format index.js

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const properties = require('properties-parser')
 const github = require('@actions/github')
 const core = require('@actions/core')
 
-const REPO = 'melkypie/resource-packs'
-const RAW_GITHUB = 'https://raw.githubusercontent.com/' + REPO
-const BRANCH = 'https://github.com/' + REPO + '/tree/'
+const OWNER = 'melkypie'
+const REPO = 'resource-packs'
+
+const RAW_GITHUB = `https://raw.githubusercontent.com/${OWNER}/${REPO}`
+const BRANCH = `https://github.com/${OWNER}/${REPO}/tree/`
+
 const PROPERTIES = 'pack.properties'
 const ICON = 'icon.png'
 const MAIN_BRANCH = 'github-actions'
 const MANIFEST = 'manifest.js'
-const TOKEN = process.env.ACCESS_TOKEN
 
-const octokit = github.getOctokit(TOKEN)
+const octokit = github.getOctokit(process.env.ACCESS_TOKEN)
 
 function parseProperties(data) {
 	if (data.startsWith('displayName')) {
@@ -28,7 +30,7 @@ async function getPackProperties(branch) {
 	let internalName = branch.name
 	let commit = branch.commit.sha
 
-	const response = await fetch(RAW_GITHUB + '/' + commit + '/' + PROPERTIES)
+	const response = await fetch(`${RAW_GITHUB}/${commit}/${PROPERTIES}`)
 	if (!response.ok || response.status != 200) {
 		if (response.status == 404) {
 			console.log(internalName, commit, PROPERTIES, 'is missing')
@@ -49,17 +51,19 @@ async function getPackProperties(branch) {
 	const props = parseProperties(data)
 	if (props != null) {
 		props['internalName'] = internalName
+
 		let tags = props['tags'].split(',')
 		if (tags[0] !== '') {
 			props['tags'] = tags
 		} else {
 			delete props['tags']
 		}
+
 		props['commit'] = commit
 		props['repo'] = BRANCH + internalName
 
-        const iconResponse = await fetch(RAW_GITHUB + '/' + commit + '/' + ICON)
-        props.hasIcon = iconResponse.status == 200
+		const iconResponse = await fetch(`${RAW_GITHUB}/${commit}/${ICON}`)
+		props.hasIcon = iconResponse.status == 200
 	} else {
 		console.log(internalName, commit, PROPERTIES, 'empty or invalid')
 	}
@@ -76,40 +80,41 @@ async function parsePackProperties(packs) {
 }
 
 async function generateManifest(properties) {
-    let json = JSON.stringify(properties)
-	console.log(JSON.stringify(properties, null, 2))
+	let json = JSON.stringify(properties)
 	let now = new Date()
 
-    const result = await octokit.rest.repos.getContent({
-        owner: 'melkypie',
-		repo: 'resource-packs',
-        path: MANIFEST,
-        ref: MAIN_BRANCH
-    })
+	console.log(JSON.stringify(properties, null, 2))
 
-    await octokit.rest.repos.createOrUpdateFileContents({
-        owner: 'melkypie',
-		repo: 'resource-packs',
-        path: MANIFEST,
-        branch: MAIN_BRANCH,
-        message: `Update ${MANIFEST} ${now.toISOString()}`,
-        content: Buffer.from(json).toString('base64'),
-        sha: result.data.sha
-    })
+	const result = await octokit.rest.repos.getContent({
+		owner: OWNER,
+		repo: REPO,
+		path: MANIFEST,
+		ref: MAIN_BRANCH
+	})
+
+	await octokit.rest.repos.createOrUpdateFileContents({
+		owner: OWNER,
+		repo: REPO,
+		path: MANIFEST,
+		branch: MAIN_BRANCH,
+		message: `Update ${MANIFEST} ${now.toISOString()}`,
+		content: Buffer.from(json).toString('base64'),
+		sha: result.data.sha
+	})
 }
 
 async function main() {
 	try {
 		const branches = await octokit.paginate(octokit.rest.repos.listBranches, {
-			owner: 'melkypie',
-			repo: 'resource-packs',
+			owner: OWNER,
+			repo: REPO,
 			per_page: 100
 		})
 
 		const packs = branches.filter((f) => f.name.startsWith('pack-'))
 		const packProperties = await parsePackProperties(packs)
-        const filteredProperties = packProperties.filter(p => p !== null)
-        await generateManifest(filteredProperties)
+		const filteredProperties = packProperties.filter((p) => p !== null)
+		await generateManifest(filteredProperties)
 
 		return Promise.resolve(true)
 	} catch (error) {
